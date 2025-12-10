3d6881a54c22d9cb8557618de3691d7d8e937b37
Pratyush Chauhan <121055922+pratyushchauhan147@users.noreply.github.com>
2025-12-10 12:57:21 +0530

    Changing Model (1.5 is depleted)

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GOOGLE_AI_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 export const generateResult = async (fea) => {
     const nprompt = `
